63be89d4a3083a116470b5fd80bcc086852fcc74
Rob Rua <rrua@andrew.cmu.edu>
2012-04-16 20:15:12 -0400

    added validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+# Validations
+validates_presence_of :content, :name, :email
+validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "Email format is not valid"
+
 end
