6abcc83029fc7eaf08f208f5a5f8f4313e015599
Rob Rua <rrua@andrew.cmu.edu>
2012-04-16 21:18:36 -0400

    finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120417000804) do
+ActiveRecord::Schema.define(:version => 20120417010129) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(:version => 20120417000804) do
     t.text     "content"
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
   end
 
   create_table "photos", :force => true do |t|
